ca96d07f742c04885cad2eb2b6b26f7a9be0ba07
NguyenVuHoangLong2012 <hoanglongdeptrai392@gmail.com>
2026-02-01 10:28:32 +0700

    Mã nguồn RunBash version 1.9
    Cập nhật mã nguồn cho RunBash version 1.9

--- RunBash.py
+++ RunBash.py
@@ -2,6 +2,7 @@ import sys
 import os
 import subprocess
 import shutil
+import pathlib
 def findBash():
 	candidates = [
 		r"C:\Program Files\Git\bin\bash.exe",
@@ -17,10 +18,17 @@ def findBash():
 			if os.path.isfile(path):
 				return path
 		return None
-BASH = os.environ.get("RUNBASH_BASH") or findBash()
+Env = os.environ.get("RUNBASH_BASH")
+if Env:
+	if os.path.isfile(Env) and os.access(Env, os.X_OK):
+		BASH = Env
+	else:
+		BASH = findBash()
+else:
+	BASH = findBash()
 def version():
-	print("RunBash version 1.8")
-def help():
+	print("RunBash version 1.9")
+def showHelp():
 	print("Using \"runbash.exe path\\script.sh [args...] or runbash.exe path\\script.bash [args...]\" to run a Bash script.")
 	print("Using \"runbash.exe --login path\\script.sh [args...] or runbash.exe --login path\\script.bash [args...]\" to run with login shell.")
 	print("Using \"runbash.exe --bash-using\" to check Bash path.")
@@ -29,38 +37,49 @@ def help():
 	print("Using \"runbash.exe --help\" to show this help.")
 	print("Using \"runbash.exe --about\" to show all info.")
 	print("Environment variables RUNBASH_BASH   Use custom bash path instead of auto-detect.")
+	print("Home page:")
+	print("https://github.com/nguyenvuhoanglong2012/runbash/")
 def bashUsing():
 	if BASH:
 		print("Bash using:")
 		print(BASH)
 	else:
 		print("Bash not found")
+		sys.exit(2)
 def showEnvironmentVariables():
 	Environment = os.environ.get("RUNBASH_BASH")
 	if  Environment is None:
 		print("Environment variables:")
 		print("Not set.")
+		sys.exit(2)
 	else:
 		print("Environment variables:")
 		print("RUNBASH_BASH:" + Environment)
 def about():
 	version()
-	help()
+	showHelp()
 	bashUsing()
 	showEnvironmentVariables()
-def sendBashScript(File, Args, Login=False):
-	if os.path.isfile(File):
-		if File.lower().endswith((".sh", ".bash")):
+def runBashScript(Win_File, Args, Login=False):
+	if os.path.isfile(Win_File):
+		if Win_File.lower().endswith((".sh", ".bash")):
 			if BASH is None:
 				print("Bash not found.")
 				sys.exit(127)
 			else:	
-				File = os.path.abspath(File)
-				SendScript = [BASH]
+				Win_File = os.path.abspath(Win_File)
+				Win_File = pathlib.PureWindowsPath(Win_File)
+				if Win_File.drive:
+					Drive = Win_File.drive[0].lower()
+					Unix_File = "/" + Drive + "/" + "/".join(Win_File.parts[1:])
+				else:
+					print("Invalid path.")
+					sys.exit(2)
+				RunScript = [BASH]
 				if Login:
-					SendScript.append("-l")
-				SendScript += [File, *Args]
-				Result = subprocess.run(SendScript, check=False, shell=False)
+					RunScript.append("-l")
+				RunScript += [Unix_File, *Args]
+				Result = subprocess.run(RunScript, check=False, shell=False)
 				sys.exit(Result.returncode)
 		else:
 			print("File is not Bash Script.")
@@ -68,21 +87,27 @@ def sendBashScript(File, Args, Login=False):
 	else:
 		print("File not found.")
 		sys.exit(1)
-def runScript(Script, Args, Login=False):
-	Script = Script.replace("\\", "/")
-	sendBashScript(Script, Args, Login)
+def stripText(Raw):
+	if not os.path.isfile(Raw):
+		Clean = Raw.strip()
+		if os.path.isfile(Clean):
+			return Clean
+		else:
+			return Raw
+	else:
+		return Raw
 def main():
 	if len(sys.argv) < 2:
 		about()
 		sys.exit(0)
 	else:
-		Script = sys.argv[1].strip()
+		Script = stripText(sys.argv[1])
 		Args = sys.argv[2:]
 		if Script.lower() == "--version":
 			version()
 			sys.exit(0)
 		elif Script.lower() == "--help":
-			help()
+			showHelp()
 			sys.exit(0)
 		elif Script.lower() == "--bash-using":
 			bashUsing()
@@ -95,12 +120,13 @@ def main():
 			sys.exit(0)
 		elif Script.lower() == "--login":
 			if len(sys.argv) < 3:
-				print("Missing parameters.")
+				print("--login missing parameters.")
 				sys.exit(2)
 			else:
-				Script = Args[0].strip()
+				Script = stripText(Args[0])
 				Args = Args[1:]
-				runScript(Script, Args, Login=True)
+				runBashScript(Script, Args, Login=True)
 		else:
-			runScript(Script, Args, Login=False)
-main()
+			runBashScript(Script, Args, Login=False)
+if __name__ == "__main__":
+	main()
